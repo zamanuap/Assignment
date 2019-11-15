e545eeeff2aa1a7d34a31a32779ffe7f7aaeb0c0
Monir <zaman_uap@yahoo.com>
2019-11-15 16:01:16 -0500

    third commit

--- CareerCloud.Pocos/CompanyDescriptionPoco.cs
+++ CareerCloud.Pocos/CompanyDescriptionPoco.cs
@@ -15,7 +15,7 @@ namespace CareerCloud.Pocos
         public Guid Company { get; set; }
 
         [Column("LanguageID")]
-        public string LanguageID { get; set; }
+        public string LanguageId { get; set; }
 
         [Column("Company_Name")]
         public string CompanyName { get; set; }
